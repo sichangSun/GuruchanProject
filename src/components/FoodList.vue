f21e82e221e1a370ebb2430962c6e9f5b630baa8
Sichang Sun <krkhssc724@gmail.com>
2023-07-19 00:09:51 +0900

    table of FoodList finished

--- src/components/FoodList.vue
+++ src/components/FoodList.vue
@@ -1,50 +1,74 @@
 <template>
-    <div class="row test">
-        <div>
-            <b-row>
-                <b-col>
-                    <div>
-        <li>
-            <div class="row foodItemStyle">
-                <div class="col-xl-1">
-                    <img src="" alt="写真">
-                </div>
-                <div class="col-xl-2">名称
-                    <div>{{ FoodList.FoodName }}</div>
-                </div>
+    <table class="col-xl-7 table">
+            <thead>
+              <tr>
+                <th scope="col">写真</th>
+                <th scope="col">食べ物</th>
+                <th scope="col">店・場所</th>
+                <th scope="col">分類</th>
+                <th scope="col">記録日付</th>
+                <th scope="col">更新日付</th>
+                <th scope="col">tag</th>
+                <th scope="col">お気に入り</th>
+                <th scope="col">操作</th>
 
-                <div class="col-xl-1">地址
-                    <div>{{ FoodList.FoodPlace }}</div>
-                    <div>
-                        <b-button>詳細へ</b-button>
-                    </div>
-                </div>
-                <div class="col-xl-1">分類
-                    <div>{{ FoodList.FoodBunnRui}}</div>
-                </div>
-                <div class="col-xl-1"> 初店行く日付
-                    <div>{{ FoodList.goToMeseTime}}</div>
-                </div>
-                <!-- ToDo 店行く日付まとめて、配列にしたほうがいい -->
-                <div class="col-xl-2"> 最後店行く日付
-                    <div>{{ FoodList.newUpdateTime}}</div>
-                </div>
-                <div class="col-xl-1"> Tag
-                    <div>{{ 'tag'}}</div>
-                </div>
-                <div class="col-xl-1"> 気に入る
-                    <div>{{ FoodList.FoodLikeFlag?FoodList.FoodLikeFlag:'なし'}}</div>
-                </div>
-                <div class="col-xl-1 delBtn">
-                   <b-button variant="danger">削除</b-button>
-                </div>
-            </div>
-        </li>
-    </div>
-                </b-col>
-            </b-row>
-        </div>
-    </div>
+              </tr>
+            </thead>
+            <tbody  >
+              <tr v-for="food in FoodList" :key="food.FoodId">
+                <th scope="row" >
+                        <td class="">
+                            <img src="" alt="写真">
+                        </td>
+                        </th>
+                        <th scope="row" >
+                        <td class="">
+                            <div>{{ food.FoodName }}</div>
+                        </td>
+                        </th>
+                        <th scope="row" >
+                        <td class="">
+                            <div>{{ food.FoodPlace }}</div>
+                            <div>
+                                <b-button>詳細へ</b-button>
+                            </div>
+                        </td>
+                        </th>
+                        <th scope="row" >
+                        <td class="">
+                            <div>{{ food.FoodBunnRui}}</div>
+                        </td>
+                        </th>
+                        <th scope="row" >
+                        <td class="">
+                            <div>{{ food.goToMeseTime}}</div>
+                        </td>
+                        </th>
+                        <!-- ToDo 店行く日付まとめて、配列にしたほうがいい -->
+                        <th scope="row" >
+                        <td class="col-xl-2">
+                            <div>{{ food.newUpdateTime}}</div>
+                        </td>
+                        </th>
+                        <th scope="row" >
+                        <td class="">
+                            <div>{{ 'tag'}}</div>
+                        </td>
+                        </th>
+                        <th scope="row" >
+                        <td class="">
+                            <div>{{ food.FoodLikeFlag?food.FoodLikeFlag:'なし'}}</div>
+                            <!-- タグ最後は配列で -->
+                        </td>
+                        </th>
+                        <th scope="row" >
+                        <td class="col delBtn">
+                        <b-button variant="danger">削除</b-button>
+                        </td>
+                    </th>
+              </tr>
+            </tbody>
+          </table>
 </template>
 
 <script>
@@ -66,19 +90,19 @@ export default {
 .foodItemStyle{
     border: 2px 1;
     /* Todo 这里写死width,但我其实想让他跟主页面一样宽,所以想用100%,但是她好像找不到他的父标签,所以没有效果,改 */
-    width: 1130px;
     height: 100px;
     background-color: gray;
-    margin: 4px;
+    margin: 7px;
 
 }
 .col-xl-4{
     background-color: aquamarine;
-    margin: 1px;
+    margin: 3px;
 }
 .col-xl-1 {
     background-color: aquamarine;
-    margin: 1px;
+    margin: 3px;
+    weight: 30px;
 }
 .delBtn{
     margin: 4px;
